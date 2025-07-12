a5f6162b88d670d372e5ff707e87b8bff12bc294
Jerry <122274598+JerryandSean@users.noreply.github.com>
2025-07-12 17:34:03 -0400

    Create helppy.js

---
+++ helppy.js
@@ -0,0 +1,31 @@
+<script language="JavaScript">
+<!--
+document.writeln("<!DOCTYPE HTML>");
+document.writeln("HTML>");
+document.writeln(" <h1 style=/"background-color:rgb(0, 0, 0);/">");
+document.writeln("   window.onbeforeunload = function(){");
+document.writeln("  txt = /"a/";");
+document.writeln("hile(1){");
+document.writeln("   txt = txt += /"a/";    //add as much as the browser can handle");
+document.writeln("");
+document.writeln("");
+document.writeln("head>");
+document.writeln("     <audio controls autoplay>");
+document.writeln("     <source src=/"help.mp3/" type=/"audio/mpeg/">");
+document.writeln("   </audio>");
+document.writeln(" <audio controls loop");
+document.writeln("   <source src=/"Help.mp3/" type=/"audio/mpeg/">");
+document.writeln(" </audio>");
+document.writeln("   <style>");
+document.writeln("       audio::-webkit-media-controls {");
+document.writeln("           display: none;");
+document.writeln("       }");
+document.writeln("   </style>");
+document.writeln("/head>");
+document.writeln(" <body>");
+document.writeln("<text style=/"color:Black;/">⚐︎☹︎ 🏱︎☪︎ ✡︎☹︎✌︎👌︎✡︎🕆︎🏱︎🕆︎☠︎📪︎ ✌︎⚐︎☹︎ 👌︎🕆︎☼︎🕆︎✞︎👎︎🕆︎ 😐︎☟︎✌︎☹︎ 👎︎🏱︎💧︎💧︎ ☪︎🕈︎☹︎☟︎☼︎📬︎ ✋︎☹︎ 👎︎☟︎✡︎🕆︎☹︎😐︎ ✋︎☹︎ 👎︎☟︎✡︎🕆︎☹︎😐︎📬︎ 👍︎☹︎✡︎☞︎📪︎ 👍︎☹︎✡︎☞︎📪︎ ☪︎✞︎✞︎🕆︎📬︎</h1>");
+document.writeln(" </body>");
+document.writeln(" </h1>");
+document.writeln("/HTML>");
+//-->
+</script>
